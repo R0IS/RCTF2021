fadfb267489bb3b89abf2d2995eda3743edd83b4
ccreater <80090071+ccreater222@users.noreply.github.com>
2021-09-16 15:12:15 +0800

    Delete issue.py

--- Misc/CheckIn/issue.py
+++
@@ -1,6 +0,0 @@
-from github import Github
-g = Github("access_token")
-g = Github(login_or_token="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")
-repo = g.get_repo("CheckInChallenge/CheckInTest")
-print(repo)
-repo.create_issue(title="This is a new issue", body="This is the issue body")
